8e1e68d70a83f1dc206e65d6602e9a232064ca86
Saranya Narayana <sgunasegaram@gmail.com>
2026-02-17 21:16:21 -0500

    added script 02

---
+++ scripts/02_qc_filtering.R
@@ -0,0 +1,9 @@
+############################################################
+# Quality filtering of TCGA-BRCA RNA-seq data
+# Project: Breast Cancer Immune Heterogeneity
+# Author: Saranya Narayana
+# Purpose:
+#   - Download TCGA-BRCA RNA-seq data
+#   - Prepare count matrix and metadata
+#   - Filter low-expression genes
+############################################################
