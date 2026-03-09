e529cdbf81ee2b36ecad95f56cd425604da89aec
Saranya Narayana <sgunasegaram@gmail.com>
2026-03-09 15:30:50 -0400

    Modified

--- scripts/04_preprossing.R
+++ scripts/04_preprossing.R
@@ -75,6 +75,7 @@ qc_metrics_sample <- data.frame(
 cat("qc_metrics_sample", dim(qc_metrics_sample), "\n")
 head(qc_metrics_sample )
 
+cat("\nSummary of qc_metrics_sample:\n")
 summary(qc_metrics_sample)
 
 # plot 1: Total count distribution
@@ -95,3 +96,47 @@ p2 <- ggplot(qc_metrics_sample, aes(x=n_genes_detected))+
     y="Number of sample")+
     theme_bw()
 ggsave("results/figures/preprocessing_plots/02_genes_detected_distribution.png", p2, width = 8, height = 6)
+
+
+# Look at quantiles
+quantile(qc_metrics_sample$total_counts / 1e6, probs = c(0.01, 0.05, 0.10, 0.90, 0.95, 0.99))
+quantile(qc_metrics_sample$n_genes_detected, probs = c(0.01, 0.05, 0.10, 0.90, 0.95, 0.99))
+
+
+#Set thresholds for filtering based on quantiles
+qc_thresholds <- list(
+  # Total counts
+  min_total_counts = quantile(qc_metrics_sample$total_counts, 0.01),
+  max_total_counts = quantile(qc_metrics_sample$total_counts, 0.99),    
+  
+  # Genes detected  
+  min_genes_detected = quantile(qc_metrics_sample$n_genes_detected, 0.01), 
+  max_genes_detected = quantile(qc_metrics_sample$n_genes_detected, 0.99)  
+)
+
+# Apply filters
+severe_outliers <- qc_metrics_sample %>%
+  filter(
+    total_counts < qc_thresholds$min_total_counts |
+    total_counts > qc_thresholds$max_total_counts |
+    n_genes_detected < qc_thresholds$min_genes_detected |
+    n_genes_detected > qc_thresholds$max_genes_detected
+  )
+
+cat("Number of severe outliers identified:", nrow(severe_outliers), "\n")#38
+
+samples_to_keep <- setdiff(colnames(counts), severe_outliers$sample)
+
+# Filter count matrices
+
+counts_clean <- counts[, samples_to_keep]
+tpm_clean <- tpm[, samples_to_keep]
+
+# Filter clinical data
+
+clinical_clean <- clinical %>%
+  filter(barcode %in% samples_to_keep)
+
+cat("Samples after QC:", ncol(counts_clean), "\n")
+cat("Retention rate:",
+round(ncol(counts_clean)/ncol(counts) * 100, 1), "%\n")**
